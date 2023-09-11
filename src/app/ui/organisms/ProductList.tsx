53b972803486b84ad70be8be7d70b2b8e5340cfa
Ryszard Wolosiuk <ryszard.wolosiuk@pwrteams.com>
2023-09-11 14:19:34 +0200

    chore: add data-test-id to the list

--- src/app/ui/organisms/ProductList.tsx
+++ src/app/ui/organisms/ProductList.tsx
@@ -7,7 +7,7 @@ type ProductListProps = {
 
 export const ProductList = ({products}: ProductListProps) => {
     return (
-        <ul className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2">
+        <ul className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2" data-testid="products-list">
             {products.map(product => <ProductListItem key={product.id} product={product} />)}
         </ul>
     )
